55cf7817b68de78f28e8ea79c4e8bed7694d9a8d
Isaac Miller <imiller@DCIMILLER.local>
2021-08-27 11:23:19 -0400

    clean up some stuff

--- front/src/Components/Error.jsx
+++ front/src/Components/Error.jsx
@@ -3,7 +3,7 @@ import '../Style/VirtualHendiInterface.css'
 
 export const Error = props => {
     return <div className="spinner-container">
-            <img src="/virtual-hendi/error.jpg" className="error"/>
+            <img src="/virtual-hendi.png" className="error" alt="error"/>
             <label>Error fetching spectrum!</label>
         </div>
 }
